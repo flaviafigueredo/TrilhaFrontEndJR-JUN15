b9f60fedc9ba8f84961cfcae11381a78913e1e83
Flávia Figueredo <flaviagfigueredo@gmail.com>
2024-07-15 19:06:23 -0300

    implementado useState para controle de itens selecionados no menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,31 +1,38 @@
+import { useState } from 'react'
 import { faRocket, faHeart, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { Container, Menu, ListItem, StyledNavLink, MenuTopic, Icon } from './styles'
 
 export const Header = () => {
+    const [selectedItem, setSelectedItem] = useState('/')
+
+    const handleMenuClick = (path) => {
+        setSelectedItem(path)
+    }
+
     return (
         <Container>
             <nav>
                 <Menu>
-                    <ListItem className="selected">
-                        <StyledNavLink to="/">
+                    <ListItem className={selectedItem === '/' ? 'selected' : ''}>
+                        <StyledNavLink to="/" onClick={() => handleMenuClick('/')}>
                             <MenuTopic>Home</MenuTopic>
                             <Icon icon={faHome} />
                         </StyledNavLink>
                     </ListItem>
-                    <ListItem>
-                        <StyledNavLink to="/sobre">
+                    <ListItem className={selectedItem === '/sobre' ? 'selected' : ''}>
+                        <StyledNavLink to="/sobre" onClick={() => handleMenuClick('/sobre')}>
                             <MenuTopic>Sobre</MenuTopic>
                             <Icon icon={faUser} />
                         </StyledNavLink>
                     </ListItem>
-                    <ListItem>
-                        <StyledNavLink to="/motivacoes">
+                    <ListItem className={selectedItem === '/motivacoes' ? 'selected' : ''}>
+                        <StyledNavLink to="/motivacoes" onClick={() => handleMenuClick('/motivacoes')}>
                             <MenuTopic>Motivações</MenuTopic>
                             <Icon icon={faRocket} />
                         </StyledNavLink>
                     </ListItem>
-                    <ListItem>
-                        <StyledNavLink to="/gostos-pessoais">
+                    <ListItem className={selectedItem === '/gostos-pessoais' ? 'selected' : ''}>
+                        <StyledNavLink to="/gostos-pessoais" onClick={() => handleMenuClick('/gostos-pessoais')}>
                             <MenuTopic>Gostos pessoais</MenuTopic>
                             <Icon icon={faHeart} />
                         </StyledNavLink>
