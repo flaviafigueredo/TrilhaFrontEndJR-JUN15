16f0a8b08e04beb473ab9498c21aaab35e8f266a
Flávia Figueredo <flaviagfigueredo@gmail.com>
2024-07-15 19:13:55 -0300

    adicionado conteúdo e estilo ao componente home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom'
+import avatar from '../../assets/avatar.jpeg'
+import { Section, Avatar, InfosContainer, Btn, Span } from "./styles"
+
 export const Home = () => {
     return (
         <>
-            <h1>Home</h1>
+            <Section>
+                <Avatar src={avatar} alt="avatar image" />
+
+                <InfosContainer>
+                    <h1>Olá, eu sou a Flávia Figueredo, <Span>desenvolvedora frontend jr.</Span></h1>
+
+                    <Link to="/sobre">
+                        <Btn>Mais sobre mim</Btn>
+                    </Link>
+                </InfosContainer>
+            </Section>
         </>
     )
 }
---
+++ src/components/home/styles.js
@@ -0,0 +1,111 @@
+import styled, { keyframes } from 'styled-components'
+import backgroundImage from '../../assets/background.jpg'
+
+export const Section = styled.section`    
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 3rem;
+    position: relative;
+    background-color: rgba(0, 0, 0, 0.3);
+
+    &::before {
+        content: "";
+        position: absolute;
+        inset: 0;
+        background-image: url(${backgroundImage});
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        z-index: -3;
+    } 
+`
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Avatar = styled.img`
+    width: 500px;
+    height: 500px;
+    border-radius: 50%;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+`
+
+export const InfosContainer = styled.div`
+    color: #fff;
+    max-width: 50rem;
+    text-transform: uppercase;
+    font-size: 2rem;
+`
+
+export const Animation = keyframes`
+    0% {
+        -webkit-transform: scale(1);
+        transform: scale(1);
+    }
+
+    50% {
+        -webkit-transform: scale(1.1);
+        transform: scale(1.1);
+    }
+
+    100% {
+        -webkit-transform: scale(1);
+        transform: scale(1);
+    }
+`
+
+export const Btn = styled.button`
+    font-size: 1.6rem;
+    text-transform: uppercase;
+    background-color: rgba(229, 57, 57, 0.5);
+    color: #fff;
+    padding: 1rem 2rem;
+    border: 1px solid #e53939;
+    border-radius: 5rem;
+
+    margin-top: 3rem;
+    cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    transition: 0.8s ease-in-out;
+
+    &:hover {
+        -webkit-animation: ${Animation} 0.8s ease-in-out infinite both;
+        animation: ${Animation} 0.8s ease-in-out infinite both;
+        background-color: #e53939;
+  }
+`
+
+export const Typing = keyframes`
+    from {
+        width: 0;
+    }
+
+    to {
+        width: 100%;
+    }
+`
+
+export const BlinkCursor = keyframes`
+    from {
+        border-right-color: #fff;
+    }
+
+    to {
+        border-right-color: transparent;
+    }
+`
+
+export const Span = styled.span`
+    font-family: "Roboto Mono", monospace;
+    border-right: 2px solid #fff;
+    white-space: nowrap;
+    overflow: hidden;
+    text-align: center;
+    display: block;
+    animation: ${BlinkCursor} 500ms steps(40) infinite normal, ${Typing} 4s steps(40) 1s normal both;
+`
