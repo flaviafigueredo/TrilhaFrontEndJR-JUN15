4719c9435b0c5165eb8d007e182b18b76565379a
Flávia Figueredo <flaviagfigueredo@gmail.com>
2024-07-16 14:54:31 -0300

    adicionado conteúdo e estilos ao componente About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,82 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReact, faJsSquare, faCss3Alt, faHtml5, faGithub, faGitAlt } from '@fortawesome/free-brands-svg-icons'
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
 import '../../transitions.css'
+import { BackgroundAnimation } from '../background-animation/BackgroundAnimation'
+import { Section, H1, ColorHighlight, PersonalInfos, H2, IconContainer, Skills, Education, ListItem, Year, Icon, Paragraph, H3, Span } from './styles'
 
 export const About = () => {
     return (
         <>
-            <h1>About</h1>
+            <BackgroundAnimation />
+            <Section>
+                <H1>Sobre <ColorHighlight>mim</ColorHighlight></H1>
+
+                <PersonalInfos>
+                    <p> 
+                        Olá, me chamo <ColorHighlight>Flávia Figueredo</ColorHighlight> e tenho 32 anos. Desde muito jovem, nutro uma paixão pela tecnologia. Minha jornada acadêmica iniciou-se com
+                        uma formação em <ColorHighlight>Produção Audiovisual</ColorHighlight>, mas meu verdadeiro sonho sempre foi explorar o fascinante
+                        mundo da programação. Decidi transformar esse sonho em realidade, embarcando em uma jornada de
+                        estudos dedicados ao <ColorHighlight>frontend</ColorHighlight>.
+                    </p>
+
+                    <p>
+                        Com uma visão de longo prazo, concluí uma <ColorHighlight>Pós-graduação em Desenvolvimento FullStack</ColorHighlight> e, atualmente, estou nos momentos finais de uma <ColorHighlight>Pós-graduação em Engenharia de Software</ColorHighlight>. Essa trajetória reflete meu comprometimento e determinação na transição de carreira para a área de Desenvolvimento FrontEnd, contribuindo com inovação e criatividade para a vanguarda da tecnologia.
+                    </p>
+                </PersonalInfos>
+
+                <Skills>
+                    <H2>Minhas habilidades</H2>
+
+                    <IconContainer>
+                        <FontAwesomeIcon icon={faReact} />
+                        <FontAwesomeIcon icon={faJsSquare} />
+                        <FontAwesomeIcon icon={faCss3Alt} />
+                        <FontAwesomeIcon icon={faHtml5} />
+                        <FontAwesomeIcon icon={faGithub} />
+                        <FontAwesomeIcon icon={faGitAlt} />
+                    </IconContainer>
+                </Skills>
+
+                <Education>
+                    <H2>Educação</H2>
+                    <ListItem>
+                        <Year>
+                            <Icon><FontAwesomeIcon icon={faBriefcase} /></Icon>
+                            <Paragraph>2024</Paragraph>
+                        </Year>
+
+                        <H3>Pós-Graduação em Engenharia de Software <Span>- Faculdade Descomplica</Span></H3>
+                    </ListItem>
+
+                    <ListItem>
+                        <Year>
+                            <Icon><FontAwesomeIcon icon={faBriefcase} /></Icon>
+                            <Paragraph>2023 - Presente</Paragraph>
+                        </Year>
+
+                        <H3>Curso em Desenvolvimento FrontEnd <Span>- DevQuest</Span></H3>
+                    </ListItem>
+
+                    <ListItem>
+                        <Year>
+                            <Icon><FontAwesomeIcon icon={faBriefcase} /></Icon>
+                            <Paragraph>2023</Paragraph>
+                        </Year>
+
+                        <H3>Pós-Graduação em Desenvolvimento FullStack <Span>- Faculdade Descomplica</Span></H3>
+                    </ListItem>
+
+                    <ListItem>
+                        <Year>
+                            <Icon><FontAwesomeIcon icon={faBriefcase} /></Icon>
+                            <Paragraph>2014</Paragraph>
+                        </Year>
+
+                        <H3>Produção Audiovisual <Span>- PUCRS</Span></H3>
+                    </ListItem>
+                </Education>
+            </Section>
         </>
     )
 }
---
+++ src/components/about/styles.js
@@ -0,0 +1,110 @@
+import styled from "styled-components"
+
+export const Section = styled.section`
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 3rem;
+    min-height: 100vh;
+    max-width: 80rem;
+    margin: 0 auto;
+    padding: 5rem 0;
+`
+
+export const H1 = styled.h1`
+    font-size: 4rem;
+    text-transform: uppercase;
+`
+
+export const ColorHighlight = styled.span`
+    color: #e53939;
+`
+
+export const PersonalInfos = styled.div`
+    font-family: 'Roboto Mono', monospace;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    font-size: 2rem;
+    line-height: 4rem;
+    text-align: justify;
+`
+
+export const H2 = styled.h2`
+    font-size: 3rem;
+    text-transform: uppercase;
+`
+
+export const Skills = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 2rem;
+    width: 100%;
+`
+
+export const IconContainer = styled.div`
+    display: flex;
+    gap: 3rem;
+    font-size: 5rem;
+`
+
+export const Education = styled.ul`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 2rem;
+    width: 100%;
+`
+
+export const ListItem = styled.li`
+    font-family: 'Roboto Mono', monospace;
+    border-left: 1px solid #fff;
+    padding-left: 2rem;
+    margin-left: 2rem;
+`
+
+export const Year = styled.div`
+    display: flex;
+    align-items: center;
+    position: relative;
+`
+
+export const Icon = styled.span`
+    background-color: #e53939;
+    font-size: 1.6rem;
+    width: 4rem;
+    height: 4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5rem;
+    position: absolute;
+    left: -4rem;
+    top: 0;
+`
+
+export const Paragraph = styled.p`
+    margin: 0 2rem 2rem;
+    padding: .7rem 1.4rem;
+    font-size: 1.2rem;
+    color: #CFCFCF;
+    text-transform: uppercase;
+    background-color: rgba(37, 37, 37, 0.44);
+    border-radius: 2rem;
+    font-weight: 700;
+`
+
+export const H3 = styled.h3`
+    font-size: 1.8rem;
+    text-transform: uppercase;
+    margin-left: 2rem;
+`
+
+export const Span = styled.span`
+    color: #CFCFCF;
+    opacity: .8;
+    font-size: 1.5rem;
+`
